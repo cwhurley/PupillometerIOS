dc175a3301980f198936a41d3d32381046c51536
cwhurley <cwhurley94@gmail.com>
2017-07-12 11:35:10 +1000

    Project Created

---
+++ Pupillometer/Pupillometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pupillometer
+//
+//  Created by Chris Hurley on 12/7/17.
+//  Copyright © 2017 Chris Hurley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
