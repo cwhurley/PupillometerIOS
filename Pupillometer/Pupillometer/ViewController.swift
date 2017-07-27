2682192b72996a800ecf321d66d016b5f6df8c5a
QiuchiChen <30280098+QiuchiChen@users.noreply.github.com>
2017-07-27 19:00:04 +1000

    status bar color changed
    changed the color of the status bar to fit the whole design color

--- Pupillometer/Pupillometer/ViewController.swift
+++ Pupillometer/Pupillometer/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
