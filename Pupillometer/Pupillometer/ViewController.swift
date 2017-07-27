dc89e1fcca10c774e3a7187e92ec5ccec3480e35
cwhurley <cwhurley94@gmail.com>
2017-07-27 19:14:39 +1000

    Camera View
    Adds the Camera view to the detection page

--- Pupillometer/Pupillometer/ViewController.swift
+++ Pupillometer/Pupillometer/ViewController.swift
@@ -7,12 +7,76 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var captureSession = AVCaptureSession()
+    var previewLayer = AVCaptureVideoPreviewLayer()
+    var captureDevice: AVCaptureDevice?
+    var frontCamera: Bool = true
+    var stillImageOutput: AVCaptureStillImageOutput = AVCaptureStillImageOutput()
+    
+    @IBOutlet var cameraView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        frontCamera(frontCamera)
+        
+        if captureDevice != nil {
+            beginSession()
+        }
+
+    }
+    
+    func beginSession(){
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.cameraView.layer.addSublayer(previewLayer)
+        previewLayer.frame = self.cameraView.layer.bounds
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        captureSession.startRunning()
+        stillImageOutput.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+        if captureSession.canAddOutput(stillImageOutput){
+            captureSession.addOutput(stillImageOutput)
+        }
+        
+    }
+    
+    func frontCamera(_ front:Bool){
+        let devices = AVCaptureDevice.devices()
+        
+        
+        
+        
+        do {
+            try captureSession.removeInput(AVCaptureDeviceInput(device: captureDevice))
+        }
+        catch {
+            print("error")
+        }
+        
+        for device in devices!{
+            
+            if((device as AnyObject).hasMediaType(AVMediaTypeVideo)){
+                if front{
+                    if (device as AnyObject).position == AVCaptureDevicePosition.back {
+                        captureDevice = device as? AVCaptureDevice
+                        
+                        do {
+                            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+                            
+                        }
+                        catch {
+                            
+                        }
+                        break
+                    }
+                    
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +84,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startButton(_ sender: Any) {
+    }
 
 }
 
